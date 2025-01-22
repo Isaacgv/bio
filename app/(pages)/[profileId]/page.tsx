947f7fc8c9f1e350f32e9174bd188b0d67d2f085
Isaac <isaacgonzalesv@gmail.com>
2025-01-22 21:41:13 +0100

    add upgrade page

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "@/app/components/commons/project-card";
 import TotalVisits from "@/app/components/commons/total-visits";
 import UserCard from "@/app/components/commons/user-card";
 import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default async function ProfilePage({
   params,
@@ -13,9 +14,13 @@ export default async function ProfilePage({
     <div className="relative h-screen flex p-20 overflow-hidden">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
         <span>You are using the trial version.</span>
-        <button className="text-accent-green font-bold">
-            Upgrade now!
-        </button>
+        <Link href={`/${profileId}/upgrade`}>
+            <button className="text-accent-green font-bold">
+                Upgrade now!
+            </button>
+        </Link>
+
+
       </div>
       <div className="w-1/2 flex justify-center h-min">
         <UserCard />
