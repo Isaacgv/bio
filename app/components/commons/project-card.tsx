c44f930881c5ed740a79d1297c8cc6dcdbf2ddf0
Isaac <isaacgonzalesv@gmail.com>
2025-01-22 21:37:30 +0100

    add profile interfaz

--- app/components/commons/project-card.tsx
+++ app/components/commons/project-card.tsx
@@ -1,6 +1,6 @@
 export default function ProjectCard() {
     return (
-      <div className="w-[430px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
+      <div className="w-[340px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
         <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
           <img
             src="/project1.jpg"
@@ -10,7 +10,7 @@ export default function ProjectCard() {
         </div>
         <div className="flex flex-col gap-2">
           <span className="uppercase text-xs font-bold text-accent-green">
-            10 cliques
+            10 clicks
           </span>
           <div className="flex flex-col">
             <span className="text-white font-bold">Projeto 1</span>
